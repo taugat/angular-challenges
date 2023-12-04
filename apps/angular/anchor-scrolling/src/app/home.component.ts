950a768d0b3a085cb474aebbdb1cbf00d0061e20
taugat <auguste.hatton@gmail.com>
2023-12-04 13:16:29 +0000

    fix: replacing nav-button by rooterLink

--- apps/angular/anchor-scrolling/src/app/home.component.ts
+++ apps/angular/anchor-scrolling/src/app/home.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
-import { NavButtonComponent } from './nav-button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [NavButtonComponent],
+  imports: [RouterLink],
   selector: 'app-home',
   template: `
-    <nav-button href="/foo" class="fixed top-3 left-1/2">Foo Page</nav-button>
+    <button routerLink="/foo" class="fixed top-3 left-1/2">Foo Page</button>
     <div id="top" class="h-screen bg-gray-500">
       Empty
-      <nav-button href="#bottom">Scroll Bottom</nav-button>
+      <button routerLink="" fragment="bottom">Scroll Bottom</button>
     </div>
     <div id="bottom" class="h-screen bg-blue-300">
       I want to scroll each
-      <nav-button href="#top">Scroll Top</nav-button>
+      <button routerLink="" fragment="top">Scroll Top</button>
     </div>
   `,
 })
